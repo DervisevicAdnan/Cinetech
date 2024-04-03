b7fd034682b9cc21571290969286d01eb5b95ba3
Adi Drakovac <129613464+adidrakovac1@users.noreply.github.com>
2024-04-03 15:30:20 +0200

    C# aplikacija
    Dodavanje c# web aplikacije

---
+++ CineTech/CineTech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
