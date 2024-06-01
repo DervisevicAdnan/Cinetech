7a1dfa97cf32d7ec6d2a5e5af1a1376098a666a3
DervisevicAdnan <44082091+DervisevicAdnan@users.noreply.github.com>
2024-06-01 20:52:55 +0200

    izmjene za automatsku rolu

--- CineTech/CineTech/obj/Debug/net8.0/CineTech.AssemblyInfo.cs
+++ CineTech/CineTech/obj/Debug/net8.0/CineTech.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0589639de8d6862419272aaed737dd36050959c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a985e40cc8fa1ee0a6aca53650cfc377eed1d440")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
