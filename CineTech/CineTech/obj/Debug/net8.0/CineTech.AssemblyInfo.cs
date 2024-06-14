81531f180959b9fa7c68aa62e667a67d0789a152
DervisevicAdnan <44082091+DervisevicAdnan@users.noreply.github.com>
2024-06-14 09:50:52 +0200

    Neki stilski detalji

--- CineTech/CineTech/obj/Debug/net8.0/CineTech.AssemblyInfo.cs
+++ CineTech/CineTech/obj/Debug/net8.0/CineTech.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006b4928891a02b89ab98736a496c4d734e94605")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d996d2e317b702ee9c888f8d5c7b52d7bbf3ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
